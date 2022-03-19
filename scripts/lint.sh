cc2f4ac558b3f656336b568fbb12342f18bf7737
chenk <hen.keinan@gmail.com>
2022-03-19 11:19:53 +0200

    chore: generate mocks
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.27.0
-export PATH=$GOPATH/bin:$PATH
-export PATH=$PATH:/root/go/bin
 golangci-lint run -v  > lint.xml
