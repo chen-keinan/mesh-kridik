92f63884629b5be17bc8bbe91deaf27dca831ff3
chenk <hen.keinan@gmail.com>
2022-03-19 10:49:57 +0200

    chore: generate mocks
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,7 +2,9 @@
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.27.0
 ## need to generate mock before lint
 go get -d github.com/golang/mock/mockgen@v1.6.0
+echo "get mockgen"
 go get -d github.com/golang/mock/mockgen
+export GOPATH=~/go
 export PATH=$GOPATH/bin:$PATH
 export PATH=$PATH:/root/go/bin
 go generate ./...
